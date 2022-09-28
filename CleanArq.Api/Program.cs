b4dcd48e28c650c7c3fb7a797fd93525344335af
Nicolas Undiano <nicolasundiano@gmail.com>
2022-09-27 21:39:18 -0300

    fix

--- CleanArq.Api/Program.cs
+++ CleanArq.Api/Program.cs
@@ -23,6 +23,8 @@ var app = builder.Build();
         app.UseSwaggerUI();
     }
 
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
     app.UseAuthentication();
     app.UseAuthorization();
