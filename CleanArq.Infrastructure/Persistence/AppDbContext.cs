0572e66ff7f55aaaf559aea9ae9923f019422987
Nicolas Undiano <nicolasundiano@gmail.com>
2022-09-30 00:40:09 -0300

    refac

--- CleanArq.Infrastructure/Persistence/AppDbContext.cs
+++ CleanArq.Infrastructure/Persistence/AppDbContext.cs
@@ -1,54 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Reflection;
 using Microsoft.AspNetCore.Http;
-using CleanArq.Domain.Entities.Common;
 using CleanArq.Domain.Entities.User;
 
 namespace CleanArq.Infrastructure.Persistence;
 
-public class AppDbContext : DbContext
+public class AppDbContext : BaseDbContext
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor)
+        : base(options, httpContextAccessor)
     {
-        _httpContextAccessor = httpContextAccessor;
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Address> Addresses { get; set; } = null!;
-
-    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
-    {
-        var entries = ChangeTracker
-            .Entries()
-            .Where(e => e.Entity is BaseEntity && (
-                    e.State == EntityState.Added
-                    || e.State == EntityState.Modified));
-
-        foreach (var entityEntry in entries)
-        {
-            if (entityEntry.State == EntityState.Added)
-            {
-                ((BaseEntity)entityEntry.Entity).CreatedAt = DateTime.UtcNow;
-                ((BaseEntity)entityEntry.Entity).CreatedBy = this._httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "MyApp";
-            }
-            else
-            {
-                Entry((BaseEntity)entityEntry.Entity).Property(p => p.CreatedAt).IsModified = false;
-                Entry((BaseEntity)entityEntry.Entity).Property(p => p.CreatedBy).IsModified = false;
-            }
-
-            ((BaseEntity)entityEntry.Entity).ModifiedAt = DateTime.UtcNow;
-            ((BaseEntity)entityEntry.Entity).ModifiedBy = this._httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "MyApp";
-        }
-
-        return await base.SaveChangesAsync(cancellationToken);
-    }
 }
---
+++ CleanArq.Infrastructure/Persistence/BaseDbContext.cs
@@ -0,0 +1,50 @@
+﻿using CleanArq.Domain.Entities.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CleanArq.Infrastructure.Persistence;
+
+public class BaseDbContext : DbContext
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public BaseDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var entries = ChangeTracker
+            .Entries()
+            .Where(e => e.Entity is BaseEntity && (
+                    e.State == EntityState.Added
+                    || e.State == EntityState.Modified));
+
+        foreach (var entityEntry in entries)
+        {
+            if (entityEntry.State == EntityState.Added)
+            {
+                ((BaseEntity)entityEntry.Entity).CreatedAt = DateTime.UtcNow;
+                ((BaseEntity)entityEntry.Entity).CreatedBy = this._httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "MyApp";
+            }
+            else
+            {
+                Entry((BaseEntity)entityEntry.Entity).Property(p => p.CreatedAt).IsModified = false;
+                Entry((BaseEntity)entityEntry.Entity).Property(p => p.CreatedBy).IsModified = false;
+            }
+
+            ((BaseEntity)entityEntry.Entity).ModifiedAt = DateTime.UtcNow;
+            ((BaseEntity)entityEntry.Entity).ModifiedBy = this._httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "MyApp";
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
