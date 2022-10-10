e69edb7e9e2c64931306bf722aacad45baafca30
Nicolas Undiano <nicolasundiano@gmail.com>
2022-10-10 15:26:23 -0300

    fix

--- CleanArq.Api/Controllers/UserController.cs
+++ CleanArq.Api/Controllers/UserController.cs
@@ -33,7 +33,6 @@ public class UserController : ApiController
     [AllowAnonymous]
     public async Task<IActionResult> GetUsers([FromQuery] GetUserListPaginatedCommand query)
     {
-        return Ok("asdd");
         ErrorOr<PaginatedList<UserDto>> result = await _mediator.Send(query);
 
         return result.Match(
